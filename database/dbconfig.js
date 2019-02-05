46cc6001af406a46239aba841776507b1176a2f4
lndubose <lndubose13@gmail.com>
2019-02-04 22:21:36 -0600

    Change knexfile and config for deployment

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,5 +1,5 @@
-const knex = require('knex');
+const dbEngine = process.env.DB || 'development';
 
-const knexConfig = require('../knexfile.js');
+const knexConfig = require('../knexfile.js')[dbEngine];
 
-module.exports = knex(knexConfig.development);
+module.exports = require('knex')(knexConfig);
--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,15 @@
 // Update with your config settings.
 
+require('dotenv').config();
+
+const localPg = {
+    host: 'localhost',
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
     development: {
         client: 'sqlite3',
@@ -10,6 +20,21 @@ module.exports = {
         migrations: {
             directory: './database/migrations',
             tableName: 'dbmigrations',
+        },
+        seeds: {
+            directory: './database/seeds',
+        },
+    },
+    production: {
+        client: 'pg',
+        connection: dbConnection,
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'dbmigrations',
+            directory: './database/migrations',
         },
         seeds: {
             directory: './database/seeds',
