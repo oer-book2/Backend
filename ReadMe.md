e7a6179f6293ffa244f71043c2fd211cf66aa63e
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-02-06 17:56:15 -0500

    read me added

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,28 @@
-Site is deployed at https://oerbookr2.herokuapp.com/
+## OER Bookr 2
+
+- Site is deployed at: 
+### https://oerbookr2.herokuapp.com/
+
+- A `POST` request to this route will lead to a register page where users can register to join the OER Bookr site to begin use
+
+### https://oerbookr2.herokuapp.com/oerbooker/register
+
+- A `POST` request to this route will allow user to login to begin reviewing, rating, and using OER Bookr
+
+### https://oerbookr2.herokuapp.com/oerbooker/login
+
+- A `GET` request to this route will return a list of textbooks available for teachers and other educators to review and leave ratings on 
+
+### https://oerbookr2.herokuapp.com/oerbooker/textbooks
+
+- A `GET` request to this route will return a specific textbook based on the id requested along with the  review and ratings for that textbook
+
+### https://oerbookr2.herokuapp.com/oerbooker/textbooks/{textbookId}
+
+- Users can edit their rating or review by making a `PUT` request to this route 
+
+### https://oerbookr2.herokuapp.com/oerbooker/reviews/{reviewID}
+
+- To delete a rating users can go to this route by making a `DELETE` request to this route 
+
+### https://oerbookr2.herokuapp.com/oerbooker/reviews/{reviewID}
--- database/seeds/text-books.js
+++ database/seeds/text-books.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('text-books').insert([
-        {id: 1, title: 'rowValue1', author: 'authors', subject: 'math', description: 'descriptions'},
-        {id: 2, title: 'rowValue2', author: 'authors', subject: 'math', description: 'descriptions'},
-        {id: 3, title: 'rowValue3', author: 'authors', subject: 'math', description: 'descriptions'}
+        {id: 1, title: '100 People: A World Portrait', author: 'authors', subject: 'Worl History', description: 'Overview: This website gives you the opportunity see the world through different people all over the world on a variety of topics. Watch videos, see lesson plans about global issues and looking at it from a lense of focus on 100 people.'},
+        {id: 2, title: '4th Grade Students Investigate Electric Circuits Through Construction and Illustration', author: 'Lorraine Aaland ', subject: 'Electronic Technology', description: 'Overview: As an activity related to FOSS unit Magnetism and Electricity, 4th grade science students use a computer download to explore electrical circuits and to generate illustrations of electrical circuits for physical models built in class'},
+        {id: 3, title: 'G All vs. Only some', author: 'Illustrative Mathematics ', subject: 'math', description: 'This is a task from the Illustrative Mathematics website that is one part of a complete illustration of the standard to which it is aligned. Each task has at least one solution and some commentary that addresses important asects of the task and its potential use'}
       ]);
     });
 };
