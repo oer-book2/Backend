8969e46acab7f91966cefe915f8ae3ee89edbd74
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-02-07 23:19:32 -0500

    rating to 0

--- routes/login.js
+++ routes/login.js
@@ -13,22 +13,6 @@ router.use(express.json());
 router.use(cors());
 router.use(helmet());
 
-function authenticate(req, res, next) {
-    const token = req.headers.authorization;
-
-    if(token) {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-            if(err) {
-                res.status(401).json({message: `invalid token`})
-            } else {
-                req.decodedToken = decodedToken;
-                next();
-            }
-        })
-    } else {
-        res.status(401).json({ message: `no token provided` })
-    }
-};
 
 function generateToken(user) {
     const payload = {
