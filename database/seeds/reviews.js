5edd19fff546fba10e4dbfa615388a22f45e58bd
lndubose <lndubose13@gmail.com>
2019-02-07 18:19:58 -0600

    Stop review seeds

--- database/seeds/reviews.js
+++ database/seeds/reviews.js
@@ -1,31 +1,31 @@
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('reviews')
-        .truncate()
-        .then(function() {
-            // Inserts seed entries
-            return knex('reviews').insert([
-                {
-                    id: 1,
-                    comment: 'comments go here',
-                    textbook_id: 1,
-                    rating: 3,
-                    name: 'Luna',
-                },
-                {
-                    id: 2,
-                    comment: 'comments go here',
-                    textbook_id: 2,
-                    rating: 3,
-                    name: 'Luna',
-                },
-                {
-                    id: 3,
-                    comment: 'comments go here',
-                    textbook_id: 3,
-                    rating: 3,
-                    name: 'Luna',
-                },
-            ]);
-        });
-};
+// exports.seed = function(knex, Promise) {
+//     // Deletes ALL existing entries
+//     return knex('reviews')
+//         .truncate()
+//         .then(function() {
+//             // Inserts seed entries
+//             return knex('reviews').insert([
+//                 {
+//                     id: 1,
+//                     comment: 'comments go here',
+//                     textbook_id: 1,
+//                     rating: 3,
+//                     name: 'Luna',
+//                 },
+//                 {
+//                     id: 2,
+//                     comment: 'comments go here',
+//                     textbook_id: 2,
+//                     rating: 3,
+//                     name: 'Luna',
+//                 },
+//                 {
+//                     id: 3,
+//                     comment: 'comments go here',
+//                     textbook_id: 3,
+//                     rating: 3,
+//                     name: 'Luna',
+//                 },
+//             ]);
+//         });
+// };
