57b3ebb4c81f6d9c6688f55575be7ae36fda85c5
lndubose <lndubose13@gmail.com>
2019-02-07 18:24:50 -0600

    Messing with seeds

--- database/seeds/reviews.js
+++ database/seeds/reviews.js
@@ -5,27 +5,27 @@ exports.seed = function(knex, Promise) {
         .then(function() {
             // Inserts seed entries
             return knex('reviews').insert([
-                // {
-                //     id: 1,
-                //     comment: 'comments go here',
-                //     textbook_id: 1,
-                //     rating: 3,
-                //     name: 'Luna',
-                // },
-                // {
-                //     id: 2,
-                //     comment: 'comments go here',
-                //     textbook_id: 2,
-                //     rating: 3,
-                //     name: 'Luna',
-                // },
-                // {
-                //     id: 3,
-                //     comment: 'comments go here',
-                //     textbook_id: 3,
-                //     rating: 3,
-                //     name: 'Luna',
-                // },
+                {
+                    id: 1,
+                    comment: 'comments go here',
+                    textbook_id: 1,
+                    rating: 3,
+                    name: 'Luna',
+                },
+                {
+                    id: 2,
+                    comment: 'comments go here',
+                    textbook_id: 2,
+                    rating: 3,
+                    name: 'Luna',
+                },
+                {
+                    id: 3,
+                    comment: 'comments go here',
+                    textbook_id: 3,
+                    rating: 3,
+                    name: 'Luna',
+                },
             ]);
         });
 };
